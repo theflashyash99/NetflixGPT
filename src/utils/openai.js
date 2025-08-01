e4b6a8a319328efd23aa9ac5a93dae9567805b59
Yash Jaiswal <100846396+theflashyash99@users.noreply.github.com>
2025-08-01 16:31:50 +0530

    Delete src/utils/openai.js

--- src/utils/openai.js
+++
@@ -1,9 +0,0 @@
-import OpenAI from "openai";
-import { OPENAI_GPT } from "./constants";
-
- const client = new OpenAI({
-    apiKey: OPENAI_GPT,
-    dangerouslyAllowBrowser: true,
- });
-
- export default client;
