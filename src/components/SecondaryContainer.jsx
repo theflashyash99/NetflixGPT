fff3d18da53d9ecb92bf5e40a463c7f944cf1289
Yash Jaiswal <100846396+theflashyash99@users.noreply.github.com>
2025-08-01 16:10:57 +0530

    Update SecondaryContainer.jsx

--- src/components/SecondaryContainer.jsx
+++ src/components/SecondaryContainer.jsx
@@ -5,8 +5,8 @@ import { useSelector } from "react-redux";
 const SecondaryContainer = () => {
   const movies = useSelector((store) => store.movies);
   return (
-    <div className=" bg-black ">
-    <div className="-mt-85 pl-12 relative z-20">
+    <div className=" bg-black sm:mt-20 md:mt-20 ">
+    <div className="  md:-mt-85   pl-4 md:pl-12 relative z-20">
       <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
       <MovieList title={"Top Rated"} movies={movies.topRatedMovies} />
       <MovieList title={"Popular"} movies={movies.popularMovies} />
