547ba834f76ae30a869357ce84322b675cc615e2
Yash Jaiswal <100846396+theflashyash99@users.noreply.github.com>
2025-08-01 16:15:25 +0530

    Update GptMovieSuggestion.jsx

--- src/components/GptMovieSuggestion.jsx
+++ src/components/GptMovieSuggestion.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import MovieList from './MovieList';
 
 const GptMovieSuggestion = () => {
+  const gpt = useSelector(store => store.gpt);
+  const {movieName , movieResults} = gpt;
+  if(!movieName) return null; 
+ 
   return (
-    <div>GptMovieSuggestion</div>
-  )
-}
+    <div className="p-4 m-4 text-white bg-black opacity-90">
+      {movieName.map((movie, index) => (
+        <MovieList 
+          key={index} 
+          title={movie} 
+          movies={movieResults[index]} 
+        />
+      ))}
+    </div>
+  );
+};
 
 export default GptMovieSuggestion
+//now
