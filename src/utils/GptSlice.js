86765f1c3e29a30e4de1d76fc0119c19f176184b
Yash Jaiswal <100846396+theflashyash99@users.noreply.github.com>
2025-08-01 16:04:21 +0530

    Update GptSlice.js

--- src/utils/GptSlice.js
+++ src/utils/GptSlice.js
@@ -4,12 +4,20 @@ const gptSlice = createSlice({
   name: "gpt",
   initialState: {
     showGptSearch: false,
+    gptMovies : null,
+    moviesName : null,
+    moviesResults : null
   },
   reducers: {
     toggleGptSearchView: (state, action) => {
       state.showGptSearch = !state.showGptSearch;
     },
+    addGptMoviesResults : (state,action) => {
+      const { moviesName , moviesResults} = action.payload;
+      state.moviesName = moviesName,
+      state.moviesResults = moviesResults
+    }
   },
 });
-export const { toggleGptSearchView } = gptSlice.actions;
+export const { toggleGptSearchView, addGptMoviesResults } = gptSlice.actions;
 export default gptSlice.reducer;
